9bb82468675c68dab9c3c9639e999d06d37861c8
hesrondev <hesron@live.fr>
2015-12-10 14:30:26 +0100

    ProfessorService: implémenation, ajout des méthodes ProfessorServiceImpl: implémentation

--- src/main/java/com/ingesup/labojava/service/ProfessorService.java
+++ src/main/java/com/ingesup/labojava/service/ProfessorService.java
@@ -1,5 +1,15 @@
 package com.ingesup.labojava.service;
 
+import java.util.List;
+
+import com.ingesup.labojava.bean.Professor;
+
 public interface ProfessorService {
+	
+	public void addProfessor(Professor prof);
+	public void updateProfessor(Professor prof);
+	public List<Professor> listProfessors();
+	public Professor getProfessor(String email, String pass);
+	public void removeProfessor(Long id);
 
 }
--- src/main/java/com/ingesup/labojava/service/ProfessorServiceImpl.java
+++ src/main/java/com/ingesup/labojava/service/ProfessorServiceImpl.java
@@ -1,5 +1,46 @@
 package com.ingesup.labojava.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ingesup.labojava.bean.Professor;
+import com.ingesup.labojava.dao.ProfessorDAO;
+
+@Service
+@Transactional
 public class ProfessorServiceImpl implements ProfessorService{
+	
+	private ProfessorDAO professorDAO;
+
+	public void setPersonDAO(ProfessorDAO professorDAO) {
+		this.professorDAO = professorDAO;
+	}
+
+	@Override
+	public void addProfessor(Professor prof) {
+		this.professorDAO.addPerson(prof);
+	}
+
+	@Override
+	public void updateProfessor(Professor prof) {
+		this.professorDAO.updatePerson(prof);
+	}
+
+	@Override
+	public List<Professor> listProfessors() {
+		return this.professorDAO.listProfessors();
+	}
+
+	@Override
+	public Professor getProfessor(String email, String pass) {
+		return this.professorDAO.getProfessor(email, pass);
+	}
+
+	@Override
+	public void removeProfessor(Long id) {
+		this.removeProfessor(id);
+	}
 
 }
