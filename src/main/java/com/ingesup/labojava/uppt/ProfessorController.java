9becb5e63a14edbd6706bc91c48ff9d0ff23e54e
hesrondev <hesron@live.fr>
2015-12-10 20:40:39 +0100

    Pom.xml: renommage de l'artifact id et du groupe id Packages: renommage de labojava et labojava.uppt

--- src/main/java/com/ingesup/labojava/uppt/ProfessorController.java
+++ src/main/java/com/ingesup/labojava/uppt/ProfessorController.java
@@ -1,4 +1,4 @@
-package com.ingesup.labojava;
+package com.ingesup.labojava.uppt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
