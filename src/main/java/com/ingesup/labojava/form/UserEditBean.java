4cadd7360e180b6f5d5738398eff9aa5a37983ac
souadbdi <belmehdi.souad78@gmail.com>
2016-04-26 23:45:17 +0200

    commit UserEditBean et flaty.css

---
+++ src/main/java/com/ingesup/labojava/form/UserEditBean.java
@@ -0,0 +1,17 @@
+package com.ingesup.labojava.form;
+
+import java.sql.Blob;
+
+public class UserEditBean {
+	
+	protected Blob picture;
+
+	public Blob getPicture() {
+		return picture;
+	}
+
+	public void setPicture(Blob picture) {
+		this.picture = picture;
+	}
+
+}
